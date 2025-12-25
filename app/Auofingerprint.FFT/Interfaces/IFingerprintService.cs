fa7aca3bc7e7a26271642e22162362ae9333ca88
stynix <orangegirlrussen@gmail.com>
2025-12-25 19:18:46 +0300

    Delete app/Auofingerprint.FFT directory

--- app/Auofingerprint.FFT/Interfaces/IFingerprintService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Audiofingerprint.Interfaces
-{
-	public interface IFingerprintService
-	{
-		string GenerateFingerprint(string path, string filePathForSave);
-		double CompareFingerprints(byte[] firstPath, byte[] secondPath);
-
-	}
-}
